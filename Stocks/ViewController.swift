bed73b1847e6e0b15d76e57a0be6585f21a88b50
Oleg991 <o.n.eremenko@gmail.com>
2020-08-28 15:34:28 +0300

    Initial Commit

---
+++ Stocks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by Олег Еременко on 28.08.2020.
+//  Copyright © 2020 Oleg Eremenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

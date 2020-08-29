3605a831c41096b36bd009f990a7c2d14cd0788e
Oleg991 <o.n.eremenko@gmail.com>
2020-08-29 11:40:57 +0300

    Load companies data success

--- Stocks/ViewController.swift
+++ Stocks/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MessageUI
 
 class ViewController: UIViewController {
     @IBOutlet weak var companyNameLabel: UILabel!
@@ -17,20 +18,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var priceChangeLabel: UILabel!
     @IBOutlet weak var logoImageView: UIImageView!
     
+    private lazy var devEmail = "support_stocks@gmail.com"
 // MARK: Companies for UIPickerView
+//
+//    private lazy var companies = [
+//        "Apple": "AAPL",
+//        "Microsoft": "MSFT",
+//        "Google": "GOOG",
+//        "Amazon": "AMZN",
+//        "Facebook": "FB"
+//    ]
     
-    private lazy var companies = [
-        "Apple": "AAPL",
-        "Microsoft": "MSFT",
-        "Google": "GOOG",
-        "Amazon": "AMZN",
-        "Facebook": "FB"
-    ]
+    private var companiesArray = [Company]()
 
 // MARK: Request stocks data and image
     
     private func requestQuote(for symbol: String) {
         let token = "sk_2300dc06c77a4de5a7b9b4301594f733"
+        
         guard let url1 = URL(string: "https://cloud.iexapis.com/stable/stock/\(symbol)/quote?token=\(token)"),
             let url2 = URL(string: "https://cloud.iexapis.com/stable/stock/\(symbol)/logo?token=\(token)") else {
             return
@@ -40,7 +45,8 @@ class ViewController: UIViewController {
             if let data = data, (response as? HTTPURLResponse)?.statusCode == 200, error == nil {
                 self.parseQuote(from: data)
             } else {
-                print("Network error!")
+                self.showALert(title: "Network error", message: "Check your internet connection and tap OK to reload company data")
+                print("Network error! Could not get quote data")
             }
         }
         
@@ -50,14 +56,46 @@ class ViewController: UIViewController {
             if let data = data, (response as? HTTPURLResponse)?.statusCode == 200, error == nil {
                 self.parseImage(from: data)
             } else {
-                print("Network error!")
+                print("Network error! Could not get image URL")
             }
         }
         
         dataTask2.resume()
     }
     
+    private func requestCompanies() {
+        let token = "sk_2300dc06c77a4de5a7b9b4301594f733"
+        guard let url = URL(string: "https://cloud.iexapis.com/stable/stock/market/list/mostactive?token=\(token)") else { return }
+        
+        let dataTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data, (response as? HTTPURLResponse)?.statusCode == 200, error == nil {
+                self.parseCompanies(from: data)
+            } else {
+                self.showALert(title: "Network error", message: "Check your internet connection and tap OK to reload companies")
+                print("Network error! Could not get companies data")
+            }
+        }
+        
+        dataTask.resume()
+        
+        return
+    }
+    
+    private func parseCompanies(from data: Data) {
+        let jsonData = try? JSONDecoder().decode([Company].self, from: data)
+        guard let companiesData = jsonData else { return }
+        companiesArray = companiesData
+        DispatchQueue.main.async {
+            self.companyPickerView.reloadAllComponents()
+            self.requestQuoteUpdate()
+        }
+        
+    }
+    
     private func parseQuote(from data: Data) {
+        let jsonErrorTitle = "Invalid data"
+        let jsonErrorMessage = "Tap OK to send a bug report"
+        
         do {
             let jsonObject = try JSONSerialization.jsonObject(with: data)
             
@@ -66,7 +104,10 @@ class ViewController: UIViewController {
                 let companyName = json["companyName"] as? String,
                 let companySymbol = json["symbol"] as? String,
                 let price = json["latestPrice"] as? Double,
-                let priceChange = json["change"] as? Double else { return print("Invalid JSON") }
+                let priceChange = json["change"] as? Double else {
+                    print("Invalid JSON")
+                    return showALert(title: jsonErrorTitle, message: jsonErrorMessage)
+            }
             
             DispatchQueue.main.async { [weak self] in
                 self?.displayStockInfo(companyName: companyName, companySymbol: companySymbol, price: price, priceChange: priceChange)
@@ -77,14 +118,19 @@ class ViewController: UIViewController {
     }
     
     private func parseImage(from data: Data) {
+        let jsonErrorTitle = "Invalid data"
+        let jsonErrorMessage = "Tap OK to send a bug report"
+        
         do {
             let jsonObject = try JSONSerialization.jsonObject(with: data)
             
             guard
                 let json = jsonObject as? [String: Any],
                 let stringURL = json["url"] as? String,
-                let imageURL = URL(string: stringURL) else { return print("Invalid JSON") }
-            
+                let imageURL = URL(string: stringURL) else {
+                    print("Invalid JSON")
+                    return showALert(title: jsonErrorTitle, message: jsonErrorMessage)
+            }
                 logoImageView.load(url: imageURL)
         } catch  {
             print("JSON parsing error: " + error.localizedDescription)
@@ -106,7 +152,6 @@ class ViewController: UIViewController {
     
     private func requestQuoteUpdate() {
         activityIndicator.startAnimating()
-        
         let labelArray = [companyNameLabel, companySymbolLabel, priceLabel, priceChangeLabel]
         for x in labelArray {
             x?.text = "..."
@@ -119,11 +164,49 @@ class ViewController: UIViewController {
                 }
             }
         }
+        
         logoImageView.image = UIImage(named: "brand")
+        logoImageView.backgroundColor = .white
         
         let selectedRow = companyPickerView.selectedRow(inComponent: 0)
-        let selectedSymbol = Array(companies.values)[selectedRow]
+        let selectedSymbol = companiesArray[selectedRow].symbol
         requestQuote(for: selectedSymbol)
+    }
+    
+    // MARK: - ALert
+    
+    private func showALert(title: String, message: String){
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default) { [weak self] action in
+                if title != "Network error" {
+                    let subject = "Report a problem in app"
+                    self?.sendEmail(subject: subject)
+                } else {
+                    self?.requestQuoteUpdate()
+                }
+            }
+            alert.addAction(okAction)
+            
+            self.present(alert, animated: true)
+            self.activityIndicator.stopAnimating()
+        }
+
+    }
+    
+    // MARK: - Report a bug Email
+    
+    private func sendEmail(subject: String) {
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients([devEmail])
+            mail.setMessageBody("<p> Invalid data error in Alert title </p>", isHTML: true)
+            mail.setSubject(subject)
+            present(mail, animated: true)
+        } else {
+            print("error")
+        }
     }
     
     // MARK: - Lifecycle
@@ -136,7 +219,7 @@ class ViewController: UIViewController {
         
         activityIndicator.hidesWhenStopped = true
         
-        requestQuoteUpdate()
+        requestCompanies()
     }
     
 }
@@ -149,7 +232,7 @@ extension ViewController: UIPickerViewDataSource {
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return companies.keys.count
+        return companiesArray.count
     }
 }
 
@@ -157,11 +240,20 @@ extension ViewController: UIPickerViewDataSource {
 
 extension ViewController: UIPickerViewDelegate {
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return Array(companies.keys)[row]
+//        return Array(companies.keys)[row]
+        return companiesArray[row].companyName
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         requestQuoteUpdate()
+    }
+}
+
+// MARK: - MessageUI
+
+extension ViewController: MFMailComposeViewControllerDelegate {
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true)
     }
 }
 
